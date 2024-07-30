698ede6229a991131b702c607f87c2014c201661
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-07-30 23:29:09 +0100

    ✨  F02: Navbar > Install routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/order/main/Home'
+import Navbar from './components/pages/order/navbar/Navbar'
 
 function App() {
 
   return (
     <div>
-      <Home/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
