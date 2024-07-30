ff79f925222de0b430425d024a579429bf12cb6e
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-07-31 00:06:58 +0100

    ✨  F02: Add fakeData

---
+++ src/fakeData/fakeData.jsx
@@ -0,0 +1,33 @@
+// navLinksConfig.js
+export const navLinks = [
+    { 
+        id: "1", 
+        path: "/", 
+        label: "Accueil" 
+    },
+    { 
+        id: "2", 
+        path: "/apropos", 
+        label: "Apropos" 
+    },
+    { 
+        id: "3", 
+        path: "/formations", 
+        label: "Formations" 
+    },
+    { 
+        id: "4", 
+        path: "/evenements", 
+        label: "Evenements" 
+    },
+    { 
+        id: "5", 
+        path: "/blogs", 
+        label: "Blogs" 
+    },
+    { 
+        id: "6", 
+        path: "/contact", 
+        label: "Contact" 
+    },
+];
