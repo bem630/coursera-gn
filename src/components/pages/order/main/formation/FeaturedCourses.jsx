e4ca3d3b37b85188322e3f12d242ef763119a3c1
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-07-31 13:49:40 +0100

    💄   F03: Formation > FeaturedCourses > Add Layout

--- src/components/pages/order/main/formation/FeaturedCourses.jsx
+++ src/components/pages/order/main/formation/FeaturedCourses.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components';
 
 export default function FeaturedCourses() {
   return (
-    <div>FeaturedCourses</div>
+    <FeaturedStyled>FeaturedCourses</FeaturedStyled>
   )
 }
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const FeaturedStyled = styled.div`
+  padding: 2rem;
+  background-color: #f9f9f9;
+  animation: ${fadeIn} 0.5s ease-in;
+`;
