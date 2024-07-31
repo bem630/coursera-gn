36a93478872e0c659cc47aec03a3439dc42a4558
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-07-31 02:48:52 +0100

    ✨  F03: Home > Add <Category/>

--- src/components/pages/order/main/Home.jsx
+++ src/components/pages/order/main/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
 import styled from 'styled-components';
+import Category from './formation/Category';
 
 export default function Home() {
   return (
     <HomeStyled>
         <Banner/>
+        <Category/>
     </HomeStyled>
   )
 }
