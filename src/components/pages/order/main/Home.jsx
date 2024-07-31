36e102f1fd17efc142fff037c9019565603e0128
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-07-31 13:43:35 +0100

    ✨  F03: Home > Add FeaturedCourses

--- src/components/pages/order/main/Home.jsx
+++ src/components/pages/order/main/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Banner from './Banner'
 import styled from 'styled-components';
 import Category from './formation/Category';
+import FeaturedCourses from './formation/FeaturedCourses';
 
 export default function Home() {
   return (
     <HomeStyled>
         <Banner/>
         <Category/>
+        <FeaturedCourses/>
     </HomeStyled>
   )
 }
