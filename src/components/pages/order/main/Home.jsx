e44460ed4be941ed572f707c1313f310161ed8ea
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-01 22:40:29 +0100

    ✨  F04:  Error > Ajout final de tous les composants de HomePage

--- src/components/pages/order/main/Home.jsx
+++ src/components/pages/order/main/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Banner from './Banner'
 import styled from 'styled-components';
+import Category from './formation/Category';
+import FeaturedCourses from './formation/FeaturedCourses';
+import Testimonials from './Testimonials';
+import PartnerLogos from './PartnerLogos';
+import LatestBlogs from './LatestBlogs';
+import UpcomingEvents from './UpcomingEvents';
+import CallToAction from './CallToAction';
+import FAQSection from './FAQSection';
 
 export default function Home() {
   return (
     <HomeStyled>
         <Banner/>
+        <Category/>
+        <FeaturedCourses/>
+        <Testimonials/>
+        <PartnerLogos/>
+        <LatestBlogs/>
+        <UpcomingEvents/>
+        <CallToAction/>
+        <FAQSection/>
     </HomeStyled>
   )
 }
